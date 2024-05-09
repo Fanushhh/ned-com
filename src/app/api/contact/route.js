249c5668dda51a9fd25a676a5beb2b43ff301df9
Fanush <fgabriel.mihailescu@gmail.com>
2024-05-09 14:03:59 +0300

    fix: file upload

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -6,43 +6,39 @@ import { join } from "path";
 
 
 export async function POST(req, res) {
-    const data = await req.formData();
-    const file = data.get('photoAttachments')
-    const phoneNr = data.get('phoneNr');
-    const message = data.get('propertyMessage');
+  const data = await req.formData();
+  const file = data.get('photoAttachments');
+  const phoneNr = data.get('phoneNr');
+  const message = data.get('propertyMessage');
 
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
 
-    const path = join(process.cwd(),'public', 'uploads', file.name);
-    await writeFile(path, buffer);
-    console.log(`open ${path} to see your uploaded file`) ;  
+  const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD,
+      },
+  });
 
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-        },
-    });
-    await transporter.sendMail({
-        from: phoneNr,
-        to: process.env.EMAIL,
-        subject: "New message from contact form",
-        text: `Phone number: ${phoneNr} \n Message: ${message}`,
-        attachments: [
-        {
-            filename: file.name,
-            path: './public/uploads/' + file.name,
-            contentType: "image/jpeg", // Change to the appropriate content type based on your file type
-        },
-        ],
-    });
-    await unlink(path);
+  await transporter.sendMail({
+      from: phoneNr,
+      to: process.env.EMAIL,
+      subject: "New message from contact form",
+      text: `Phone number: ${phoneNr} \n Message: ${message}`,
+      attachments: [
+          {
+              filename: file.name,
+              content: buffer, // Attach file content directly
+              contentType: "image/jpeg", // Change to the appropriate content type based on your file type
+          },
+      ],
+  });
 
   return Response.json({
-    message: `File has been uploaded`,
-    ok: true,
+      message: `File has been uploaded and sent via email`,
+      ok: true,
   });
 }
 
